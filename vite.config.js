728acd7fb64db6c68ec2b0b6b152190eac94d41f
Mustapha377 <143838046+Mustapha377@users.noreply.github.com>
2024-12-29 21:46:49 +0100

    messages

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-   plugins: [react()],
- base: '/Todo-list-page/',
- server: {  
-  port: 3000, // Change the dev server port if needed
-},
- build: {
-  rollupOptions: {
-    external: ['react/jsx-runtime']
-  }
-}
-})
+  base: '/Todo-list-app/', // Adjust this for GitHub Pages
+  server: {  
+    port: 3000, // Change the dev server port if needed
+  },
+  build: {
+    outDir: 'dist', // Output directory
+  },
+});
